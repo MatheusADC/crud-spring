fcc68ddf2368a19011808b10ee535033bc45423d
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-18 15:48:19 -0300

    Add: Read courses (API HTTP GET)

--- src/main/java/com/matheus/crud_spring/CrudSpringApplication.java
+++ src/main/java/com/matheus/crud_spring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.matheus.crud_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.matheus.crud_spring.model.Course;
+import com.matheus.crud_spring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,14 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			courseRepository.save(c);
+		};
+	}
 }
