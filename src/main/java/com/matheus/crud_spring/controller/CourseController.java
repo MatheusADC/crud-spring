802b81a1d838f5fcc395dd7a29833dd5759edf1e
MatheusSuper <matheusamaralsuper@gmail.com>
2025-03-07 20:09:38 -0300

    Add: Pagination

--- src/main/java/com/matheus/crud_spring/controller/CourseController.java
+++ src/main/java/com/matheus/crud_spring/controller/CourseController.java
@@ -11,15 +11,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheus.crud_spring.dto.CourseDTO;
+import com.matheus.crud_spring.dto.CoursePageDTO;
 import com.matheus.crud_spring.service.CourseService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 @Validated
 @RestController
@@ -33,8 +37,8 @@ public class CourseController {
     }
 
     @GetMapping
-    public List<CourseDTO> list() {
-        return courseService.list();
+    public CoursePageDTO list(@RequestParam(defaultValue = "0") @PositiveOrZero int page, @RequestParam(defaultValue = "10") @Positive @Max(100) int pageSize) {
+        return courseService.list(page, pageSize);
     }
 
     @GetMapping("/{id}")
